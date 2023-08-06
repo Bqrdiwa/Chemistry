940e778eae83ae25e9aff2ee8f47aa8b5ba73b70
Bqrdiwa <rajabibardia353@yahoo.com>
2023-08-06 17:23:46 +0430

    Update views.py

--- main/authenticate/views.py
+++ main/authenticate/views.py
@@ -79,7 +79,7 @@ def Register(request):
                     
                     # phone_number vertification
                         #wrong numbers
-                    if re.search(r'(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4|5|6|7|8|9]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}',phone_number) == None:
+                    if re.search(r'(0|\+98)?([ ]|-|[()]){0,2}9[0|1|2|3|4|5|6|7|8|9]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}',phone_number) == None:
                         error_list['reg_phone'] = 'شماره موبایل باید با عدد 9 شروع شود'
 
                         # already exist
